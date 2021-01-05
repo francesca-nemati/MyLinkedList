d3ebd39cc4d3e338658b73d718f6b531c0cdd709
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2021-01-05 14:18:57 -0500

    set instance variables

--- Node.java
+++ Node.java
@@ -1,5 +1,9 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
+ public Node(String value){
+   data = value;
+   next = null;
+   prev = null;
+ }
  private String data;
  private Node next,prev;
  //write get/set methods for all three instance variables.
