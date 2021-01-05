2ed42322f2882d006253031141d82f161bfc4be1
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2021-01-05 16:32:58 -0500

    added get and set methods for all instance variables

--- Node.java
+++ Node.java
@@ -1,10 +1,36 @@
 public class Node{
- public Node(String value){
-   data = value;
-   next = null;
-   prev = null;
- }
- private String data;
- private Node next,prev;
- //write get/set methods for all three instance variables.
+
+  private String data;
+  private Node next,prev;
+
+  public Node(String value){
+    data = value;
+  }
+
+//write get/set methods for all three instance variables.
+
+  public String getData() {
+    return data;
+  }
+
+  public void setData(String newData) {
+    data = newData;
+  }
+
+  public Node getNext() {
+    return next;
+  }
+
+  public void setNext(Node newNext) {
+    next = newNext;
+  }
+
+  public Node getPrev() {
+    return prev;
+  }
+
+  public void setPrev(Node newPrev) {
+    prev = newPrev;
+  }
+
 }
